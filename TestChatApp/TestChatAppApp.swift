5ba5ab5315b3e5ef06f2d63d6744f65debab5035
iMtRo <maxmudovilyos75@gmail.com>
2022-02-16 14:37:08 +0500

    Initial Commit

---
+++ TestChatApp/TestChatAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestChatAppApp.swift
+//  TestChatApp
+//
+//  Created by user on 16/02/22.
+//
+
+import SwiftUI
+
+@main
+struct TestChatAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
